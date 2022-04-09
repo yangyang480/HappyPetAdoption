ba7fe44ca5bc24437a8c365b07c2de5f5e6ac1a7
yangyang480 <yangyangzhao1213@gmail.com>
2022-04-08 20:42:05 -0500

    Update UserDAO.java

--- src/main/java/capstone/HappyPetAdoption/database/Dao/UserDAO.java
+++ src/main/java/capstone/HappyPetAdoption/database/Dao/UserDAO.java
@@ -1,4 +1,4 @@
-package capstone.HappyPetAdoption.database.DAO;
+package capstone.HappyPetAdoption.database.Dao;
 
 import capstone.HappyPetAdoption.database.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
