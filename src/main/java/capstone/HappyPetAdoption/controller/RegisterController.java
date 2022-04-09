8aec05d61937068cebaef7ddce3c849c33ba987e
yangyang480 <82347891+yangyang480@users.noreply.github.com>
2022-04-08 20:08:04 -0500

    small fix on views

--- src/main/java/capstone/HappyPetAdoption/controller/RegisterController.java
+++ src/main/java/capstone/HappyPetAdoption/controller/RegisterController.java
@@ -16,4 +16,6 @@ public class RegisterController {
         return response;
     }
 
+
+
 }
--- src/main/java/capstone/HappyPetAdoption/database/Dao/UsersDAO.java
+++ src/main/java/capstone/HappyPetAdoption/database/Dao/UsersDAO.java
@@ -1,4 +1,4 @@
-package capstone.HappyPetAdoption.database.Dao;
+package capstone.HappyPetAdoption.database.DAO;
 
 import capstone.HappyPetAdoption.database.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
