ac8aeea65cdf456130f3bfdada6e007ab54ab3f7
Anthony Pearson <yangyangzhao1213@gmail.com>
2022-04-08 19:22:46 -0500

    Revert "Ignore ds store. fix and be able to connect to db"
    This reverts commit d1fd1e95ecf1c22eaeec44e1e61cc0b08e3957e4.

--- src/main/java/capstone/HappyPetAdoption/HappyPetAdoptionApplication.java
+++ src/main/java/capstone/HappyPetAdoption/HappyPetAdoptionApplication.java
@@ -2,18 +2,11 @@ package capstone.HappyPetAdoption;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.io.Serializable;
 
 @SpringBootApplication
-public class HappyPetAdoptionApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(HappyPetAdoptionApplication.class);
-	}
+public class HappyPetAdoptionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HappyPetAdoptionApplication.class, args);
