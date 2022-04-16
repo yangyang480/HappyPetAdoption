323fc973f493225cbcd89fd1797fdf1e1e949a3a
yangyang480 <82347891+yangyang480@users.noreply.github.com>
2022-04-16 14:32:26 -0500

    Add adoption service

--- src/main/java/capstone/HappyPetAdoption/Services/AdoptionService.java
+++ src/main/java/capstone/HappyPetAdoption/Services/AdoptionService.java
@@ -1,51 +1,65 @@
 package capstone.HappyPetAdoption.Services;
 
 import capstone.HappyPetAdoption.database.Dao.AdoptionDAO;
+import capstone.HappyPetAdoption.database.Entitys.Adoption;
+import capstone.HappyPetAdoption.database.Entitys.Enums.OrderStatusEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Component
 @Service
 @RequiredArgsConstructor
 public class AdoptionService {
-//
-//    @Autowired
-//    private AdoptionDAO adoptionDAO;
 
-    public void ApproveAdoption()
+    @Autowired
+    private AdoptionDAO adoptionDAO;
+
+    public Adoption approveAdoption(Adoption adoption)
     {
-
+        adoption.setOrderStatus(OrderStatusEnum.PENDING);
+        return this.adoptionDAO.save(adoption);
     }
 
-    public void CancelAdoption()
+    public Adoption cancelAdoption(Adoption adoption)
     {
-
+        adoption.setOrderStatus(OrderStatusEnum.CANCELED);
+        return this.adoptionDAO.save(adoption);
     }
 
-    public void CompleteAdoption()
+    public Adoption completeAdoption(Adoption adoption)
     {
-
+        adoption.setOrderStatus(OrderStatusEnum.COMPLETED);
+        return this.adoptionDAO.save(adoption);
     }
 
-    public void GetAdoptionById()
+    public Adoption getAdoptionById(Long id)
     {
-
+        return this.adoptionDAO.getById(id);
     }
 
-    public void GetAdoptionsByHospitalId()
+    public List<Adoption> getAdoptionsByShelterId(Long shelterId)
     {
-
+        return this.adoptionDAO.getAdoptionsByRescuerId(shelterId);
     }
 
-    public void GetAdoptionsByUserId()
+    public List<Adoption> getAdoptionsByRescuerId(Long rescuerId)
     {
-
+        return this.adoptionDAO.getAdoptionsByRescuerId(rescuerId);
     }
 
-    public void RequestAdoption()
+    public Adoption requestAdoption(Integer animalId, Integer rescuerId, Integer shelterId)
     {
+        Adoption adoption = new Adoption();
 
+        adoption.setAnimalId(animalId);
+        adoption.setRescuerId(rescuerId);
+        adoption.setShelterId(shelterId);
+        adoption.setOrderStatus(OrderStatusEnum.REQUESTED);
+
+        return this.adoptionDAO.save(adoption);
     }
 }
