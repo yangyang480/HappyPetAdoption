fa4b707acdec4a282f1655e99392c4dd4a107dd1
yangyang480 <yangyangzhao1213@gmail.com>
2022-04-16 14:32:26 -0500

    Add adoption service

--- src/main/java/capstone/HappyPetAdoption/database/Dao/AdoptionDAO.java
+++ src/main/java/capstone/HappyPetAdoption/database/Dao/AdoptionDAO.java
@@ -2,8 +2,18 @@ package capstone.HappyPetAdoption.database.Dao;
 
 import capstone.HappyPetAdoption.database.Entitys.Adoption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AdoptionDAO extends JpaRepository<Adoption, Long> {
+
+    @Query(value = "select a from Adoption a where a.shelterId = : shelterId", nativeQuery = true)
+    public List<Adoption> getAdoptionsByShelterId(@Param("shelterId") Long shelterId);
+
+    @Query(value = "select a from Adoption a where a.rescuerId = : rescuerId", nativeQuery = true)
+    public List<Adoption> getAdoptionsByRescuerId(@Param("rescuerId") Long rescuerId);
 }
